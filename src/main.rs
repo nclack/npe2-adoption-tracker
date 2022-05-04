87f7401fa523aaf16d02fe1945e4a75360daaa30
Nathan Clack <nclack@chanzuckerberg.com>
2022-05-04 10:21:43 -0700

    Filter by first commit date. Use csv supplied by Ben

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,8 @@ use futures::{
 };
 use reqwest::{Client, StatusCode};
 
-use tokio::runtime::Builder;
+use serde::Deserialize;
+use tokio::{fs::File, runtime::Builder};
 use tracing::{debug_span, info, info_span, instrument, Instrument};
 use tracing_chrome::ChromeLayerBuilder;
 use tracing_subscriber::prelude::*;
@@ -69,7 +70,7 @@ async fn analyze_plugin(client: &Client, repo: &str, branch: &str) -> Result<(bo
 }
 
 #[instrument]
-async fn run() -> Result<()> {
+async fn run1() -> Result<()> {
     let resp = reqwest::get("https://api.napari-hub.org/plugins")
         .instrument(debug_span!("napari-hub request plugin list"))
         .await?
@@ -102,7 +103,7 @@ async fn run() -> Result<()> {
             ready(if let Ok(body) = res {
                 let res: serde_json::Value =
                     serde_json::from_str(&body).expect("Expected a json body.");
-                let has_code_repository=res["code_repository"].as_str().is_some();
+                let has_code_repository = res["code_repository"].as_str().is_some();
                 let release_date =
                     DateTime::parse_from_rfc3339(res["first_released"].as_str().unwrap())
                         .expect("Could not parse as rfc3339 datetime string");
@@ -115,12 +116,14 @@ async fn run() -> Result<()> {
                 None
             })
         })
-        .fold(Stats::default(), |acc, json| {
-            let repo = json["code_repository"]
+        .map(|json| {
+            json["code_repository"]
                 .as_str()
                 .expect("missing 'code_repository' field")
                 .trim_start_matches("https://github.com/")
-                .to_owned();
+                .to_owned()
+        })
+        .fold(Stats::default(), |acc, repo| {
             let branches = ["main", "master"];
             let client = &client;
             {
@@ -140,14 +143,18 @@ async fn run() -> Result<()> {
                         }
                     }
                     if any && !is_npe2 {
-                        stats.stragglers.push(format!("https://github.com/{}",repo.clone()));
+                        stats
+                            .stragglers
+                            .push(format!("https://github.com/{}", repo.clone()));
                     }
                     if any || is_npe2 {
                         stats.success += any as usize;
                         stats.npe2 += is_npe2 as usize;
                     }
                     if !any {
-                        stats.failures.push(format!("https://github.com/{}",repo.clone()));
+                        stats
+                            .failures
+                            .push(format!("https://github.com/{}", repo.clone()));
                     }
                     stats
                 }
@@ -164,6 +171,76 @@ async fn run() -> Result<()> {
     Ok(())
 }
 
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "UPPERCASE")]
+struct Record {
+    url: String,
+    first_commit: String,
+}
+
+#[instrument]
+async fn run2() -> Result<()> {
+    let client = Client::new();
+    let stats = stream::iter(
+        csv::Reader::from_reader(include_str!("data/repos_created_after_2022-01-17.csv").as_bytes())
+            .deserialize()
+            .flat_map(|x| x) // ignore any per-row errors
+            .map(|record: Record| {
+                record
+                    .url
+                    .trim_start_matches("https://github.com/")
+                    .to_owned()
+            }), // grab the repo name
+    )
+    .fold(Stats::default(), |acc, repo| {
+        let branches = ["main", "master"];
+        let client = &client;
+        {
+            let repo = repo.clone();
+            async move {
+                let mut stats = acc;
+                let mut any = false;
+                let mut is_npe2 = false;
+                stats.total += 1;
+                for branch in branches {
+                    if let Ok((any_, is_npe2_)) = analyze_plugin(client, &repo, branch).await {
+                        any |= any_;
+                        is_npe2 |= is_npe2_;
+                        if is_npe2 {
+                            break;
+                        }
+                    }
+                }
+                if any && !is_npe2 {
+                    stats
+                        .stragglers
+                        .push(format!("https://github.com/{}", repo.clone()));
+                }
+                if any || is_npe2 {
+                    stats.success += any as usize;
+                    stats.npe2 += is_npe2 as usize;
+                }
+                if !any {
+                    stats
+                        .failures
+                        .push(format!("https://github.com/{}", repo.clone()));
+                }
+                stats
+            }
+        }
+        .instrument(info_span!("Analyze", repo = repo.as_str()))
+    })
+    .instrument(info_span!("Aggregate stats"))
+    .await;
+
+    let mut stats = stats;
+    stats.stragglers.sort();
+
+    dbg!((&stats, stats.npe2 as f32 / (stats.success as f32)));
+
+    Ok(())
+}
+
 fn main() -> Result<()> {
     let (chrome_layer, _guard) = ChromeLayerBuilder::new().include_args(true).build();
     tracing_subscriber::registry()
@@ -173,7 +250,7 @@ fn main() -> Result<()> {
         .init();
 
     let rt = Builder::new_multi_thread().enable_all().build()?;
-    rt.block_on(run())?;
+    rt.block_on(run2())?;
 
     Ok(())
 }
